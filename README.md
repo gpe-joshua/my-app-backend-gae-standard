6e1a294ce220b8d897abba2fa2ff81704b5576de
gpe-joshua <gpechoijs@gmail.com>
2020-01-29 19:44:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## 참고 URL
+
+블로그: https://dzone.com/articles/deploy-spring-boot-app-to-gcp-app-engine
+깃허브: https://github.com/mydeveloperplanet/mygoogleappengineplanet/tree/feature/standardenv
---
+++ src/main/resources/static/precache-manifest.8ebbd3a3e6d31f01c588aeff6a7074a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb49aae30ad4e24c999ba651a8b1feb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d122c23f4b0e4b87b5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5e96bd6374e1a06b2bf",
+    "url": "/static/js/2.573eda24.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.573eda24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1d122c23f4b0e4b87b5",
+    "url": "/static/js/main.475ce589.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
